245e75e265e7f6f7a36b630ec51105ecf2e183c9
Lemon-tech24 <alisangcocarl@gmail.com>
2023-07-17 22:07:21 +0800

    remove server proxy

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server:{
-    proxy: {
-      '/api': {
-           target: 'https://encouraging-pink-wig.cyclic.app',
-           changeOrigin: true,
-           secure: false,      
-           ws: true,
-       }
-    }
-  }
-
 })
